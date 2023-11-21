1a09f10658a4f637500b43407a04956da4c64592
Quydzethuong <huyphung0977@gmail.com>
2023-11-21 16:24:22 +0700

    hello
    fff

---
+++ Backend/src/main/java/Backend/model/Service.java
@@ -0,0 +1,24 @@
+package Backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Service {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    @Column(
+            name = "serviceId"
+    )
+    private Long id;
+    private String nameService;
+}
